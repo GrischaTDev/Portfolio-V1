76ea6f7ea1c31b908950a906bde807bc603d7c34
Grischa <Grischa.Taenzer@gmail.com>
2024-03-31 21:39:33 +0200

    Add new Images and update mobile menu

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function openMobileMenu() {
+    let mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.remove('d-none');
+}
+
+
+function closeMobileMenu() {
+    let mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.add('d-none');
+}
